ac35e0e88427fb78c8f6a2e23bde2f4283ab3bc9
Luis Castro <luisecastro@users.noreply.github.com>
2018-09-28 16:14:49 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import struct
 
 def string_to_array(string):
     return np.array([int(x) for x in string])
@@ -7,11 +8,10 @@ def array_to_string(array):
     return ''.join([str(x) for x in array])
 
 def binary_to_decimal(binary):
-    return int(binary,base=2)
+    return int(''.join(['0b', binary]),base=2)
 
 def fitness(chromosome):
-    chromosome_decimal = binary_to_decimal(chromosome)
-    return np.abs(chromosome_decimal**2-1023*chromosome_decimal)
+    return np.abs(chromosome**2-1023*chromosome)
 
 def selection_probability(population_fitness, minimize=True):
     reproduction_probability = population_fitness / np.mean(population_fitness)
@@ -20,3 +20,9 @@ def selection_probability(population_fitness, minimize=True):
         reproduction_probability = np.ones(len(reproduction_probability)) - reproduction_probability
         reproduction_probability /= np.sum(reproduction_probability)
     return reproduction_probability
+
+def floatToRawLongBits(value):
+	return struct.unpack('Q', struct.pack('d', value))[0]
+
+def longBitsToFloat(bits):
+	return struct.unpack('d', struct.pack('Q', bits))[0]

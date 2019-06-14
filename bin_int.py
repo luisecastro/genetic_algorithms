eec776fc31ee87353aface3425abc0c59892fa9d
Luis Castro <luisecastro@users.noreply.github.com>
2019-06-14 16:24:10 -0400

    Update bin_int.py

--- bin_int.py
+++ bin_int.py
@@ -6,3 +6,30 @@ def int_to_bin(x):
         x = x // 2
         
     return str(x) + result
+
+def dec_to_bin(x, k):
+    result = ''
+    
+    for i in range(k):
+        x = x * 2
+        y = int(x >= 1)
+        result += str(y)
+        x = x - y 
+        
+    return result
+
+def bin_to_int(x):
+    result = 0.0
+    
+    for i in range(len(x)):
+        result += int(x[-i-1])*2**i
+        
+    return result
+
+def bin_to_dec(x):
+    result = 0.0
+    
+    for i in range(len(x)):
+        result += int(x[i])/2**(i+1)
+        
+    return result
